854a415e1b9e773bb039ffb99b54e55da2603a25
Vincent Jousse <vincent@jousse.org>
2020-09-09 07:30:03 +0100

    First try at front matter parsing

--- src/markdown/parser.cr
+++ src/markdown/parser.cr
@@ -1,19 +1,38 @@
 class Markdown::Parser
   def self.extract_front_matter(content : String)
 
-    if content =~ /^<!--/
+    if content.starts_with?("<!--")
         pp "#### Dude there is a front matter"
+
+        front_matters = {} of String => String
 
         content.each_line do |line|
 
-          if line =~ /^-->/
+          content_array = line.split(":")
+
+          if content_array.size >= 2
+            # Parse ".. title: blah"
+            key = content_array[0].gsub(/\.\. /, "").strip
+
+            # @TODO: Here we should find the first : and split on it
+            # Otherwise title like "hey dude: how are you" will not work
+            value = content_array[1].strip
+
+            front_matters[key] = value
+          end
+
+          # pp content_array
+
+          if line.starts_with?("-->")
             pp "#### End of front matter"
             break
           end
 
-          pp line
+          # pp line
 
         end
+
+        pp front_matters
     end
 
   end

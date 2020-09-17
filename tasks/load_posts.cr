2e3ce86fa0da2d9036dfaaa80cd3616fcb80f92b
Vincent Jousse <vincent@jousse.org>
2020-09-17 06:28:00 +0100

    Add dir and lang parameters to the task

--- tasks/load_posts.cr
+++ tasks/load_posts.cr
@@ -1,7 +1,35 @@
 class LoadPosts < LuckyCli::Task
   summary "Loads posts from markdown files"
 
+  arg :lang, "Specify the lang of the posts to load",
+      shortcut: "-l"
+
+
+  arg :dir, "Specify the directory of the posts to load",
+      shortcut: "-d"
+
+
+  def help_message
+    <<-TEXT
+    #{summary}
+
+    -l LANG,--lang=LANG             The language of the posts. It can be 'fr' or 'en'.
+                                    Default to 'fr.
+    -l DIRECTORY,--dir=DIRECTORY    The directory to read the posts from.
+
+    example: lucky load_posts -l fr -d content/fr/
+    TEXT
+  end
+
   def call
+
+    case lang
+    when "en"
+      posts_lang = Post::Lang.new(:en)
+    else
+      posts_lang = Post::Lang.new(:fr)
+    end
+
 
     new_posts = {} of String => (Hash(String, String))
     old_posts = {} of String => Post
@@ -13,9 +41,9 @@ class LoadPosts < LuckyCli::Task
     end
 
 
-    puts "####################"
+    puts "#### Reading dir: #{dir} in #{lang}"
 
-    Dir["content/fr/*.md"].each do |filename|
+    Dir["#{dir}/*.md"].each do |filename|
 
       file_content = File.read(filename)
       hash = Digest::MD5.digest(file_content).to_slice.hexstring
@@ -51,7 +79,7 @@ class LoadPosts < LuckyCli::Task
               title: front_matter["title"],
               slug: slug,
               filename: filename,
-              lang: Post::Lang.new(:fr),
+              lang: posts_lang,
               published_at: date,
               hash: hash,
               teaser: teaser,
@@ -63,7 +91,7 @@ class LoadPosts < LuckyCli::Task
               slug: slug,
               filename: filename,
               published_at: date,
-              lang: Post::Lang.new(:fr),
+              lang: posts_lang,
               hash: hash,
               teaser: teaser,
               content: file_content)

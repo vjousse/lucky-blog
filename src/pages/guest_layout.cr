7ad4174decae0960bc5fdcf95324612ff86dbf02
Vincent Jousse <vincent@jousse.org>
2020-09-21 14:24:13 +0100

    Use tailwind color for nav bg

--- src/pages/guest_layout.cr
+++ src/pages/guest_layout.cr
@@ -27,7 +27,7 @@ abstract class GuestLayout
 
       body do
 
-        nav class: "flex items-center justify-between flex-wrap p-6 lg:px-12 border-t-1 border-b-1 border-light-gray relative bg-very-light-gray text-gray-900" do
+        nav class: "flex items-center justify-between flex-wrap p-6 lg:px-12 border-t-1 border-b-1 border-light-gray relative bg-gray-100 text-gray-900" do
           div class: "flex items-center flex-shrink-0 mr-6" do
             a class: "font-bold font-serif text-4xl tracking-tight", href: "/" do
               text "Vince's"

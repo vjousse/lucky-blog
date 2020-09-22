0099dc06cc81122d087e9d46358f9380898cdc06
Vincent Jousse <vincent@jousse.org>
2020-09-22 07:35:07 +0100

    Set lang in session

--- src/actions/mixins/lang/detect_lang.cr
+++ src/actions/mixins/lang/detect_lang.cr
@@ -4,15 +4,20 @@ module Lang::Detect
   end
 
   private def detect_lang
+
+    # Only try to detect the lang if it's not already set in session
     if !session.get?(:lang)
 
       if md = /\/(en|fr).*/.match(request.path)
         if detected_lang = md[1]?
           Log.info { {detected_lang: detected_lang} }
+
+          session.set(:lang, detected_lang)
         end
       end
-      Log.info { {request: request.path, method: request.method, path: request.path} }
     end
+
+    Log.debug { {current_lang: session.get?(:lang)} }
     continue
   end
 end

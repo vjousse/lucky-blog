67ce076670ceb759a5c9bc07edd0a540c18c8862
Vincent Jousse <vincent@jousse.org>
2020-09-23 10:06:49 +0100

    Reroute old /tech to /blog

--- src/handlers/legacy_redirect_handler.cr
+++ src/handlers/legacy_redirect_handler.cr
@@ -1,8 +1,8 @@
 class LegacyRedirectHandler
   include HTTP::Handler
-  LEGACY_ROUTES = {"/old-path" => "/new-path"}
 
   def call(context : HTTP::Server::Context)
+
 
     if md = /\/en(.*)/.match(context.request.path)
       Log.info { {match_data: md} }
@@ -16,11 +16,11 @@ class LegacyRedirectHandler
         context.response.status_code = 301
         context.response.headers["Location"] = new_path
 
-      else
-        call_next(context)
       end
+    elsif context.request.path.starts_with?("/tech/")
+      context.response.status_code = 301
+      context.response.headers["Location"] = context.request.path.gsub("/tech", "/blog")
     else
-      # Go to the next handler in the stack
       call_next(context)
     end
 

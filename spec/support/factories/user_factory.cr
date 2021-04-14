b3b690a817ea6c4869598ec5e5339bb4e925a708
Vincent Jousse <vincent@jousse.org>
2021-04-14 11:24:12 +0100

    Fix bad user factory name

--- spec/support/factories/user_factory.cr
+++ spec/support/factories/user_factory.cr
@@ -1,4 +1,4 @@
-class UserBox < Avram::Factory
+class UserFactory < Avram::Factory
   def initialize
     email "#{sequence("test-email")}@example.com"
     encrypted_password Authentic.generate_encrypted_password("password")

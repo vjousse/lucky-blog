197af094004c520f6ea82abc023ddd712ea615bc
Vincent Jousse <vincent@jousse.org>
2021-05-21 16:46:39 +0100

    Remove debug

--- src/js/app.js
+++ src/js/app.js
@@ -2,10 +2,10 @@
 
 // Rails Unobtrusive JavaScript (UJS) is *required* for links in Lucky that use DELETE, POST and PUT.
 // Though it says "Rails" it actually works with any framework.
-require("@rails/ujs").start();
+require('@rails/ujs').start();
 
 // Turbolinks is optional. Learn more: https://github.com/turbolinks/turbolinks/
-require("turbolinks").start();
+require('turbolinks').start();
 
 // If using Turbolinks, you can attach events to page load like this:
 //
@@ -19,21 +19,23 @@ import Prism from 'prismjs';
 //
 // Highlight all matching syntax
 Prism.highlightAll();
-console.log(Prism);
 
-
-
-var _paq = window._paq = window._paq || [];
+var _paq = (window._paq = window._paq || []);
 /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-_paq.push(["setDocumentTitle", document.domain + "/" + document.title]);
-_paq.push(["setCookieDomain", "*.vincent.jousse.org"]);
-_paq.push(["setDomains", ["*.vincent.jousse.org"]]);
+_paq.push(['setDocumentTitle', document.domain + '/' + document.title]);
+_paq.push(['setCookieDomain', '*.vincent.jousse.org']);
+_paq.push(['setDomains', ['*.vincent.jousse.org']]);
 _paq.push(['trackPageView']);
 _paq.push(['enableLinkTracking']);
 (function() {
-  var u="//stats.jousse.org/";
-  _paq.push(['setTrackerUrl', u+'matomo.php']);
+  var u = '//stats.jousse.org/';
+  _paq.push(['setTrackerUrl', u + 'matomo.php']);
   _paq.push(['setSiteId', '1']);
-  var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-  g.type='text/javascript'; g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+  var d = document,
+    g = d.createElement('script'),
+    s = d.getElementsByTagName('script')[0];
+  g.type = 'text/javascript';
+  g.async = true;
+  g.src = u + 'matomo.js';
+  s.parentNode.insertBefore(g, s);
 })();

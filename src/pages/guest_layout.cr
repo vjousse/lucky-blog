e11f750a74c5c99d7de5f059fc98eef6b415a702
Vincent Jousse <vincent@jousse.org>
2021-06-23 12:56:54 +0100

    Align icons

--- src/pages/guest_layout.cr
+++ src/pages/guest_layout.cr
@@ -33,7 +33,7 @@ abstract class GuestLayout
           end
           div class: "w-full block lg:flex lg:items-end lg:w-auto" do
             ul class: "flex text-lg font-sans-serif align-text-bottom items-center" do
-              li class: "mt-2" do
+              li class: "mt-1" do
                 link to: Blog::Index, class: "block mt-4 lg:inline-block lg:mt-0 hover:text-red-900" do
 
                 tag "svg", class: "h-6 w-6", fill: "none", stroke: "currentColor", viewBox: "0 0 24 24", xmlns: "http://www.w3.org/2000/svg" do
@@ -66,7 +66,7 @@ end
               li class: "ml-4 mr-4 text-gray-400" do
                 text "|"
               end
-              li class: "mt-2" do
+              li class: "mt-1" do
                 link to: Content::About, title: "About", class: "block mt-4 lg:inline-block lg:mt-0 hover:text-red-900" do
                   tag "svg", class: "h-6 w-6", fill: "none", stroke: "currentColor", viewBox: "0 0 24 24", xmlns: "http://www.w3.org/2000/svg" do
                     tag "path", d: "M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z", stroke_linecap: "round", stroke_linejoin: "round", stroke_width: "2"
@@ -76,7 +76,7 @@ end
               li class: "ml-4 mr-4 text-gray-400" do
                 text "|"
               end
-              li class: "mt-2" do
+              li class: "mt-1" do
                 link to: Rss::Index, title: "RSS Feed", class: "block mt-4 lg:inline-block lg:mt-0 hover:text-red-900" do
                   tag "svg", class: "h-6 w-6", fill: "none", stroke: "currentColor", viewBox: "0 0 24 24", xmlns: "http://www.w3.org/2000/svg" do
                     tag "path", d: "M6 5c7.18 0 13 5.82 13 13M6 11a7 7 0 017 7m-6 0a1 1 0 11-2 0 1 1 0 012 0z", stroke_linecap: "round", stroke_linejoin: "round", stroke_width: "2"
